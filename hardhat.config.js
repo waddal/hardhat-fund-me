9d9f9ad2a0e66ba33d0de000afa308d09133f290
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2022-10-22 20:55:54 -0700

    init

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomicfoundation/hardhat-toolbox")
+import "dotenv/config"
+
+const ALCHEMY_API_KEY =
+    process.env.ALCHEMY_API_KEY || "https://eth-goerli/example"
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
+
+module.exports = {
+    defaultNetwork: "hardhat",
+    solidity: "0.8.17",
+    networks: {
+        goerli: {
+            url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+            accounts: [GOERLI_PRIVATE_KEY],
+            chainId: 5,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            chainId: 31337,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+}
